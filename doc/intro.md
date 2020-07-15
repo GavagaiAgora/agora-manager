840064fb9d8c514b9e94bc94d8688c6ca7f599a3
GavagaiAgora <gavagaipip@protonmail.com>
2020-07-15 17:49:15 +0200

    Add clj project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to agora-manager
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/agora_manager/core_test.clj
@@ -0,0 +1,7 @@
+(ns agora-manager.core-test
+  (:require [clojure.test :refer :all]
+            [agora-manager.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
